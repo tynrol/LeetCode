cb07af71fc97d52027b64650f973b967189b96cf
Dmitry Tereshchenko <d.tereschenko@ctrl2go.com>
2022-09-12 03:48:53 +0300

    add 9

---
+++ 9-palindrome.go
@@ -0,0 +1,16 @@
+package Leetcode
+
+import "strconv"
+
+func isPalindrome(x int) bool {
+	r := []rune(strconv.Itoa(x))
+	i, j := 0, len(r)-1
+	for i < j {
+		if r[i] != r[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
